2c3253c8fd0dd92205071b429db34d5c7e6721cf
Michael Ye <michaelye22@gmail.com>
2024-01-21 11:48:34 -0500

    feat: working on CNN website

--- Convolutional_Neural_Network.py
+++ Convolutional_Neural_Network.py
@@ -2,3 +2,9 @@ import streamlit as st
 from streamlit.components import v1 as components
 
 st.set_page_config(layout="wide")
+
+# Load HTML file
+with open('Convolutional_Neural_Network_1.html', 'r') as file:    
+    html_content = file.read()
+
+components.html(html_content, width = None, height=1800)
