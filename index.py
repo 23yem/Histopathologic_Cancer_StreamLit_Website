a73427aa284473e8f4ddb655def08c428c3ed13a
Michael Ye <michaelye22@gmail.com>
2024-01-20 22:08:33 -0500

    refactor: test

--- index.py
+++ index.py
@@ -2,11 +2,5 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.markdown("""
-    <style>
-    body {
-        background-color: #F5F5F5;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+st.markdown("hi", unsafe_allow_html=True)
 
