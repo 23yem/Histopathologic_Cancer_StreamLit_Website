406bb82f586c77ed026e511ba03f546deb2fae83
Michael Ye <michaelye22@gmail.com>
2024-01-20 21:58:47 -0500

    docs: creating new project

---
+++ index.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.markdown("""
+    <style>
+    body {
+        background-color: #F5F5F5;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
