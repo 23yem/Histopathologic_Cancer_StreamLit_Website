cae6bf36d496a862171545e2744a29aa69a2c1bb
Michael Ye <michaelye22@gmail.com>
2024-01-20 23:59:27 -0500

    feat: almost finished with home page

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 import streamlit as st
+from streamlit.components import v1 as components
 
 st.set_page_config(layout="wide")
 
-st.markdown("hi", unsafe_allow_html=True)
+# Load HTML file
+with open('index.html', 'r') as file:    
+    html_content = file.read()
 
+components.html(html_content, width = None, height=1800)
