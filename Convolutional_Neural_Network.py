94034783f6022c8220ea7e64f23d7a48445f330e
Michael Ye <michaelye22@gmail.com>
2024-01-21 13:04:37 -0500

    feat: set up CNN page, but still need to deploy model

--- Convolutional_Neural_Network.py
+++ Convolutional_Neural_Network.py
@@ -7,4 +7,12 @@ st.set_page_config(layout="wide")
 with open('Convolutional_Neural_Network_1.html', 'r') as file:    
     html_content = file.read()
 
-components.html(html_content, width = None, height=1800)
+components.html(html_content, width = None, height=1100)
+
+
+
+# Load part 2
+with open('Convolutional_Neural_Network_2.html', 'r') as file:    
+    html_content_2 = file.read()
+
+components.html(html_content_2, width = None, height=5500)
